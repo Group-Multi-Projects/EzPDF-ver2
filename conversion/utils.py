c29396334b52bd0743b3367de585118a2dfbf934
Nguyễn Đình Thái <dinhthai0312@gmail.com>
2025-02-14 22:30:09 +0700

    delete trash file

--- conversion/utils.py
+++ conversion/utils.py
@@ -100,7 +100,7 @@ def pdf_to_html_payment(pdf_file_path,output_file_path):
     if (uploadedFileUrl != None):
         convertPdfToHtml(uploadedFileUrl, output_file_path)
 
-def html_to_pdf(html_path, output_pdf_path, api_key=None):
+def html_to_pdf_payment(html_path, output_pdf_path, api_key=None):
     """
     Converts HTML to PDF using PDF.co Web API.
 
